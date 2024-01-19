220685efec9600efcd43575f4c1926a274fc0a36
Doanda Dresta Rahma <ddoanda@gmail.com>
2024-01-19 23:37:27 +0700

    fix test models

--- test/data/models/tv/tv_response_model_test.dart
+++ test/data/models/tv/tv_response_model_test.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:ditonton/data/models/tv/tv_model.dart';
 import 'package:ditonton/data/models/tv/tv_response.dart';
-import 'package:ditonton/domain/entities/tv/tv.dart';
+
 import 'package:flutter_test/flutter_test.dart';
 
 import '../../../json_reader.dart';
@@ -25,25 +25,16 @@ void main() {
     voteCount: 1,
   );
 
-  final tTv = Tv(
-    adult: false,
-    backdropPath: "/path.jpg",
-    genreIds: [1, 2, 3, 4],
-    id: 1,
-    originCountry: ["A", "B", "C"],
-    originalLanguage: "Original Language",
-    originalName: "Original Name",
-    overview: "Overview",
-    popularity: 1.0,
-    posterPath: "/path.jpg",
-    firstAirDate: "2020-05-05",
-    name: "Name",
-    voteAverage: 1.0,
-    voteCount: 1,
-  );
+  final tTvResponseModel =
+      TvResponse(tvList: <TvModel>[tTvModel]);
+  group('fromJson', () {
+    test('should return a valid model from JSON', () async {
+      // arrange
+      final Map<String, dynamic> jsonMap =
+          json.decode(readJson('dummy_data/'))
+      // act
 
-  test('should be a subclass of Tv Entity', () async {
-    final result = tTvModel.toEntity();
-    expect(result, tTv);
+      // assert
+    });
   });
 }
