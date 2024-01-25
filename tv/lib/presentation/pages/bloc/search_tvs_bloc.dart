d11e1ebb46596a3df4cd7efd5a78df13bc9237ad
Doanda Dresta Rahma <ddoanda@gmail.com>
2024-01-26 04:11:11 +0700

    refactor: set delay for search api call after onchanged

--- tv/lib/presentation/pages/bloc/search_tvs_bloc.dart
+++ tv/lib/presentation/pages/bloc/search_tvs_bloc.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:core/domain/entities/tv/tv.dart';
 import 'package:equatable/equatable.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:tv/domain/usecases/search_tvs.dart';
 
 part 'search_tvs_event.dart';
@@ -13,20 +14,27 @@ class SearchTvsBloc extends Bloc<SearchTvsEvent, SearchTvsState> {
   final SearchTvs _searchTvs;
 
   SearchTvsBloc(this._searchTvs) : super(SearchEmpty()) {
-    on<OnQueryChanged>((event, emit) async {
-      final query = event.query;
+    on<OnQueryChanged>(
+      (event, emit) async {
+        final query = event.query;
 
-      emit(SearchLoading());
-      final result = await _searchTvs.execute(query);
+        emit(SearchLoading());
+        final result = await _searchTvs.execute(query);
 
-      result.fold(
-        (failure) {
-          emit(SearchError(failure.message));
-        },
-        (data) {
-          emit(SearchHasData(data));
-        },
-      );
-    });
+        result.fold(
+          (failure) {
+            emit(SearchError(failure.message));
+          },
+          (data) {
+            emit(SearchHasData(data));
+          },
+        );
+      },
+      transformer: debounce(const Duration(milliseconds: 500)),
+    );
+  }
+
+  EventTransformer<T> debounce<T>(Duration duration) {
+    return (events, mapper) => events.debounceTime(duration).flatMap(mapper);
   }
 }
